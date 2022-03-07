a21ddad4360964c8120e43bf2733b3bbac9ac526
Thomaz C Lourenço <thomazcl1@hotmail.com>
2022-03-07 12:18:48 -0300

    Conectando MongoDB com repository service

--- src/main/java/com/thomazcristian/workshopmongo/domain/User.java
+++ src/main/java/com/thomazcristian/workshopmongo/domain/User.java
@@ -3,10 +3,16 @@ package com.thomazcristian.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	private String id;
+	
+	@Id
 	private String name;
 	private String email;
 	
---
+++ src/main/java/com/thomazcristian/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.thomazcristian.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thomazcristian.workshopmongo.domain.User;
+import com.thomazcristian.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
