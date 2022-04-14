3a8c45888963ed422127ada5f3a05c9c67f71797
Thomaz C Lourenço <thomazcl1@hotmail.com>
2022-04-14 13:36:24 -0300

    Obtendo um post por id

---
+++ src/main/java/com/thomazcristian/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.thomazcristian.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thomazcristian.workshopmongo.domain.Post;
+import com.thomazcristian.workshopmongo.repository.PostRepository;
+import com.thomazcristian.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
